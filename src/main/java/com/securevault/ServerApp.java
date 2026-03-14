e3e03a78b979cd83c7a213643b78450f450b73e7
DESKTOP-O5IS8PT <vasjovan7@gmail.com>
2026-03-15 00:39:14 +0100

    ad

---
+++ src/main/java/com/securevault/ServerApp.java
@@ -0,0 +1,20 @@
+package com.securevault;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class ServerApp {
+
+    @GetMapping("/")
+    public String home() {
+        return "Server radi!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApp.class, args);
+    }
+}
